cff5e036a421c7458754586eaafc2a4711b7717d
Giang Nguyen <nggiang12@gmail.com>
2017-03-25 02:21:41 -0500

    dosa

--- modules/fifo/fifo.v
+++ modules/fifo/fifo.v
@@ -86,7 +86,7 @@ module fifo(/*AUTOARG*/
 
    always @(posedge clk) begin
       $display("\n curr_state: %d next_state: %d, read_ctr_en: %b, write_ctr_en: %b", curr_state, next_state, read_ctr_en, write_ctr_en);
-      $display("\n read_ctr: %d write_ctr: %d", read_ctr, write_ctr);
+      $display("\n read_ctr: %d write_ctr: %d", read_ptr, write_ptr);
 
    end
 
