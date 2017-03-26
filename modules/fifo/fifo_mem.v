8b7504f290ff99e83b1a0a0853ac4aba2771d77e
Giang Nguyen <nggiang12@gmail.com>
2017-03-26 17:43:06 -0500

    so what;s wrong damn

--- modules/fifo/fifo_mem.v
+++ modules/fifo/fifo_mem.v
@@ -14,7 +14,7 @@ module fifo_mem(
    wire [63:0] reg_out, elm0, elm1, elm2, elm3;
    //decode write signals
    decoder2_4 decoder(.in(write_ptr), .out(selected_elm));    
-   and2 add_or_nah[3:0](.out(en), .in1(add), .in2(selected_elm));
+   and2 add_or_nah[3:0](.out(en), .in1(write), .in2(selected_elm));
 
    //memaccess
    register_64bit cell0(.readdata(elm0), .clk(clk), .rst(rst), .writedata(data_in), .write(en[0]));
@@ -25,9 +25,10 @@ module fifo_mem(
     //data out
    mux4_1_64bit mux_out(.InA(elm0), .InB(elm1), .InC(elm2), .InD(elm3), .S(read_ptr), .Out(reg_out));
 
-   assign data_out = read ? reg_out : 63'b0;
-   
+   assign data_out = read ? reg_out : 63'h0000000000000000;
+
 	always @(posedge clk) begin
+		$display("\n en: %b write_ptr: %d", en);
 		$display("\n read_ptr: %d write_ptr: %d", read_ptr, write_ptr);
       	$display("\n elm0: %h elm1: %h elm2: %h elm3: %h", elm0, elm1, elm2, elm3);
    end
