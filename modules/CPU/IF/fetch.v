a5b9cbb03f5a2928a3bd326e7b779fa612025e99
giang12 <nggiang12@gmail.com>
2017-04-30 13:55:22 -0500

    Icache memory_system

--- modules/CPU/IF/fetch.v
+++ modules/CPU/IF/fetch.v
@@ -4,10 +4,15 @@ module fetch(instr, pcCurrent, pcPlusTwo, address, pc_sel, en, clk, rst);
 	input [15:0] address;
 
 	output [15:0] instr, pcCurrent, pcPlusTwo;
-    wire [15:0] next_pc;
+    wire [15:0] next_pc, mem_data_out;
+
+    wire Done, Stall, CacheHit, err;
+
+	assign instr =	Done ? mem_data_out :
+					err ? 16'b0 : 16'b0000100000000000;
 
     mux2_1_16bit pc_next_mux(	
-    	.InA(pcPlusTwo), 
+    	.InA(pcPlusTwo),
 		.InB(address), 
 		.S(pc_sel), 
 		.Out(next_pc)
@@ -20,24 +25,27 @@ module fetch(instr, pcCurrent, pcPlusTwo, address, pc_sel, en, clk, rst);
 		//input
 		.instrLen(4'h2), //pc + 2 bytes
 		.set(next_pc),
-		.en(en),
+		.en(en & Done | pc_sel),
 		.clk(clk),
 		.rst(rst)
 	);
-
-	//instr mem	
-	memory2c InstrMEM(	
-		.data_out(instr),
-		.data_in(16'b0),
-		.addr(pcCurrent), 
-		.enable(1'b1), 
-		.wr(1'b0), 
-		.createdump(1'b0), //on halt ?
-		.clk(clk), 
-		.rst(rst)
+	
+	mem_system InstrMEM(
+	   // Outputs
+	   .DataOut(mem_data_out), .Done(Done), .Stall(Stall), .CacheHit(CacheHit), .err(err), 
+	   // Inputs
+	   .Addr(pcCurrent), .DataIn(16'b0), .Rd(1'b1), .Wr(1'b0), .createdump(1'b0), .clk(clk), .rst(rst)
 	);
 
-	
-
-
+	// //instr mem	
+	// memory2c InstrMEM(
+	// 	.data_out(mem_data_out),
+	// 	.data_in(16'b0),
+	// 	.addr(pcCurrent), 
+	// 	.enable(1'b1), 
+	// 	.wr(1'b0), 
+	// 	.createdump(1'b0), //on halt ?
+	// 	.clk(clk), 
+	// 	.rst(rst)
+	// );
 endmodule
